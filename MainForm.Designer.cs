4037c9b506cf342364c6a66342583e3fdcb732e7
yenphan <phanthihaiyen93@gmail.com>
2020-09-06 13:52:19 +0700

    Create the Expense Management Application project

---
+++ MainForm.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ExpenseManagementApplication
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.lblExpenses = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rtbNote = new System.Windows.Forms.RichTextBox();
+            this.cbxType = new System.Windows.Forms.ComboBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.atpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.lblExpenses);
+            this.panel1.Controls.Add(this.lblTo);
+            this.panel1.Controls.Add(this.lblFrom);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(24, 242);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(538, 181);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(462, 132);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(117, 75);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(250, 27);
+            this.dtpTo.TabIndex = 1;
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(117, 29);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(250, 27);
+            this.dtpFrom.TabIndex = 1;
+            // 
+            // lblExpenses
+            // 
+            this.lblExpenses.AutoSize = true;
+            this.lblExpenses.Location = new System.Drawing.Point(24, 205);
+            this.lblExpenses.Name = "lblExpenses";
+            this.lblExpenses.Size = new System.Drawing.Size(72, 20);
+            this.lblExpenses.TabIndex = 0;
+            this.lblExpenses.Text = "Expenses:";
+            this.lblExpenses.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(24, 80);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(28, 20);
+            this.lblTo.TabIndex = 0;
+            this.lblTo.Text = "To:";
+            this.lblTo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(24, 34);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(46, 20);
+            this.lblFrom.TabIndex = 0;
+            this.lblFrom.Text = "From:";
+            this.lblFrom.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.rtbNote);
+            this.panel2.Controls.Add(this.cbxType);
+            this.panel2.Controls.Add(this.txtAmount);
+            this.panel2.Controls.Add(this.atpDate);
+            this.panel2.Controls.Add(this.lblNote);
+            this.panel2.Controls.Add(this.lblType);
+            this.panel2.Controls.Add(this.lblAmount);
+            this.panel2.Controls.Add(this.lblDate);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(583, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(390, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(266, 298);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // rtbNote
+            // 
+            this.rtbNote.Location = new System.Drawing.Point(111, 187);
+            this.rtbNote.Name = "rtbNote";
+            this.rtbNote.Size = new System.Drawing.Size(250, 61);
+            this.rtbNote.TabIndex = 4;
+            this.rtbNote.Text = "";
+            // 
+            // cbxType
+            // 
+            this.cbxType.FormattingEnabled = true;
+            this.cbxType.Location = new System.Drawing.Point(111, 133);
+            this.cbxType.Name = "cbxType";
+            this.cbxType.Size = new System.Drawing.Size(250, 28);
+            this.cbxType.TabIndex = 3;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(111, 73);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(250, 27);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // atpDate
+            // 
+            this.atpDate.Location = new System.Drawing.Point(111, 27);
+            this.atpDate.Name = "atpDate";
+            this.atpDate.Size = new System.Drawing.Size(250, 27);
+            this.atpDate.TabIndex = 1;
+            // 
+            // lblNote
+            // 
+            this.lblNote.AutoSize = true;
+            this.lblNote.Location = new System.Drawing.Point(19, 187);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(45, 20);
+            this.lblNote.TabIndex = 0;
+            this.lblNote.Text = "Note:";
+            this.lblNote.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(19, 133);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(43, 20);
+            this.lblType.TabIndex = 0;
+            this.lblType.Text = "Type:";
+            this.lblType.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(19, 78);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(65, 20);
+            this.lblAmount.TabIndex = 0;
+            this.lblAmount.Text = "Amount:";
+            this.lblAmount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(19, 32);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(44, 20);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "Date:";
+            this.lblDate.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Expense Management";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblExpenses;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblNote;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox rtbNote;
+        private System.Windows.Forms.ComboBox cbxType;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.DateTimePicker atpDate;
+    }
+}
+
